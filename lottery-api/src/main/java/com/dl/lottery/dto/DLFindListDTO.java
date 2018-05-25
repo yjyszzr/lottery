44abfc28c9b454e664775b6edde8464febd2120b
yjyszzr <1053195207@qq.com>
2018-05-25 14:10:55 +0800

    modified Change-Id: I35ddfe477ca07ba4612d88355e5ff7cac29cd066 Signed-off-by: yjyszzr <1053195207@qq.com>

---
+++ lottery-api/src/main/java/com/dl/lottery/dto/DLFindListDTO.java
@@ -0,0 +1,21 @@
+package com.dl.lottery.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.dl.lottery.dto.DlHallDTO.DlNavBannerDTO;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class DLFindListDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@ApiModelProperty(value = "咨讯轮播图列表")
+	public List<DlNavBannerDTO> navBanners;
+	
+	@ApiModelProperty(value = "资讯列表")
+	public PageInfo<DLArticleDTO> dlArticlePage;
+}
