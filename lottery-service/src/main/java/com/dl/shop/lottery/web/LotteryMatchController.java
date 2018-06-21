5851ea3cec6a227aa848a1fa62655905609ba985
duanyu <duanyu@caixiaomi.com>
2018-06-21 18:58:32 +0800

    限制100

--- lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryMatchController.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryMatchController.java
@@ -268,7 +268,7 @@ public class LotteryMatchController {
 		}
 		String betMoney = betInfo.getMoney();
 		Double orderMoney = Double.valueOf(betMoney);
-		if(orderMoney >= 100) {
+		if(orderMoney < 100) {
 			return ResultGenerator.genResult(LotteryResultEnum.BET_MATCH_WC.getCode(), LotteryResultEnum.BET_MATCH_WC.getMsg());
 		}
 		int canBetMoney = lotteryMatchService.canBetMoney();
@@ -426,7 +426,7 @@ public class LotteryMatchController {
 		}
 		String betMoney = betInfo.getMoney();
 		Double orderMoney = Double.valueOf(betMoney);
-		if(orderMoney >= 100) {
+		if(orderMoney < 100) {
 			return ResultGenerator.genResult(LotteryResultEnum.BET_MATCH_WC.getCode(), LotteryResultEnum.BET_MATCH_WC.getMsg());
 		}
 		int canBetMoney = lotteryMatchService.canBetMoney();
