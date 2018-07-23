776e2dcf3d6cea1b655bd169239804aaa7bc0109
yjyszzr <1053195207@qq.com>
2018-07-23 11:55:02 +0800

    modified
    Change-Id: I5d22157ae1145cda762c1236eeb3d68643f048ed
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- lottery-api/src/main/java/com/dl/lottery/param/DateStrParam.java
+++ lottery-api/src/main/java/com/dl/lottery/param/DateStrParam.java
@@ -1,7 +1,4 @@
 package com.dl.lottery.param;
-
-import org.hibernate.validator.constraints.NotBlank;
-
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -9,7 +6,6 @@ import lombok.Data;
 public class DateStrParam {
 	
     @ApiModelProperty(value = "当前日期字符串,月日为单数的时候不要带0：格式2018-3-5")
-    @NotBlank(message = "当前日期不能为空")
     private String dateStr;
 
 }
