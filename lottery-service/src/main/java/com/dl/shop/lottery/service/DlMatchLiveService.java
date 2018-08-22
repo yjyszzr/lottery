c5b5e53c9ee146e2137f0ba4bed89b329de426dc
yjyszzr <1053195207@qq.com>
2018-08-22 21:18:26 +0800

    modified
    Change-Id: Idabce196ee971d0ccb7e054400c0e05673ae01d0
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- lottery-service/src/main/java/com/dl/shop/lottery/service/DlMatchLiveService.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/service/DlMatchLiveService.java
@@ -101,7 +101,7 @@ public class DlMatchLiveService extends AbstractService<DlMatchLive> {
     		String matchLiveInfo = dlMatchLive.getMatchLiveInfo();
     		dto = this.parseMatchLineups(matchLiveInfo);
     	}else {
-        	dto.setMatchStatus(MatchStatusEnums.Fixture.getEnName());//抓取不到直播的，比赛状态就显示未开赛
+        	dto.setMatchStatus(MatchStatusEnums.Fixture.getCode());//抓取不到直播的，比赛状态就显示未开赛
     	}
     	return dto;
     }
