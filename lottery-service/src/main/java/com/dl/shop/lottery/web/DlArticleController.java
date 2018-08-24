b10c7a066c3bfffef2d0363648a0e0751fe94265
yjyszzr <1053195207@qq.com>
2018-08-24 10:30:22 +0800

    modified
    Change-Id: I7907998c5db9252b4fe2c0da7ce57c3e5660b53f
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- lottery-service/src/main/java/com/dl/shop/lottery/web/DlArticleController.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/web/DlArticleController.java
@@ -157,7 +157,7 @@ public class DlArticleController {
 		String channel = userDevice.getChannel();
 		if(StringUtils.isNotEmpty(channel)) {
 			if("c26013".equals(channel)) {//临时需求,可以删除,乐得体育的样式
-				if(bigNews.size() > 1) {
+				if(bigNews.size() > 0) {
 					bigNews = bigNews.subList(0, 1);
 				}
 			}
