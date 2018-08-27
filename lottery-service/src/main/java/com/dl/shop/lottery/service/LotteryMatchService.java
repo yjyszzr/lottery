60654545bfdce36a0a90e658dad643b86d157ea9
yjyszzr <1053195207@qq.com>
2018-08-27 16:40:28 +0800

    未结束的比赛列表优先展示正在比赛的，未开赛靠后展示
    Change-Id: I2a6f1aa4adddc57d2cc46bbf197b39f6e32c5a2e
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- lottery-service/src/main/java/com/dl/shop/lottery/service/LotteryMatchService.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/service/LotteryMatchService.java
@@ -2654,6 +2654,8 @@ public class LotteryMatchService extends AbstractService<LotteryMatch> {
 			lotteryMatchDTOList.removeIf(s->MatchStatusEnums.Played.getCode().equals(s.getMatchFinish()));
 		    List<LotteryMatchDTO> unique = lotteryMatchDTOList.stream().collect(Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparingLong(LotteryMatchDTO::getChangciId))), ArrayList::new));
 			lotteryMatchDTOList = unique;
+			//优先展示正在进行的比赛
+			unique.stream().sorted(Comparator.comparing(LotteryMatchDTO::getMatchFinish).reversed().thenComparing(LotteryMatchDTO::getChangciId));
 			String weekDay = DateUtil.getWeekByDateStr(dateStr);
 			matchSize = unique.stream().filter(s->s.getChangci().indexOf(weekDay) > -1).collect(Collectors.toList()).size();
 		}else if(queryMatchParamByType.getType().equals("1")) {//已结束
