dd9e24680ad9b4055ee59b0710a131376417d1ef
duanyu <duanyu@caixiaomi.com>
2018-08-31 14:48:14 +0800

    add apiModelproperty

--- lottery-api/src/main/java/com/dl/lottery/param/LeagueListByGroupIdParam.java
+++ lottery-api/src/main/java/com/dl/lottery/param/LeagueListByGroupIdParam.java
@@ -11,6 +11,6 @@ public class LeagueListByGroupIdParam implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@ApiModelProperty(value = "联赛分组Id")
+	@ApiModelProperty(value = "联赛分组Id：0热门，1欧洲，2美洲，3亚洲，4国际")
 	private Integer groupId;
 }
