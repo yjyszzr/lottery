5277bbbbfe5ad28e82531d26d5303649cbc46b80
liweijie <liweijie@caixiaomi.com>
2018-10-16 17:28:25 +0800

    lottery新增接口

---
+++ lottery-service/src/main/java/com/dl/shop/lottery/service/ArtifiPrintLotteryUserLoginService.java
@@ -0,0 +1,26 @@
+package com.dl.shop.lottery.service;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ArtifiPrintLotteryUserLoginService {
+
+	@Resource
+	private StringRedisTemplate stringRedisTemplate;
+
+	/**
+	 * 重置用户活跃时间
+	 * 
+	 * @param mobile
+	 */
+	public void updateUserStatus(String mobile) {
+		stringRedisTemplate.opsForValue().set(mobile, "1", 900, TimeUnit.SECONDS);
+	};
+}
---
+++ lottery-service/src/main/java/com/dl/shop/lottery/web/ArtifiPrintLotteryUserLoginController.java
@@ -0,0 +1,118 @@
+package com.dl.shop.lottery.web;
+
+import io.swagger.annotations.ApiOperation;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dl.base.result.BaseResult;
+import com.dl.base.result.ResultGenerator;
+import com.dl.base.util.JSONHelper;
+import com.dl.base.util.RegexUtil;
+import com.dl.lottery.enums.MemberEnums;
+import com.dl.member.api.ISMSService;
+import com.dl.member.api.IUserLoginService;
+import com.dl.member.dto.UserLoginDTO;
+import com.dl.member.param.LoginLogParam;
+import com.dl.member.param.SmsParam;
+import com.dl.member.param.UserLoginWithSmsParam;
+import com.dl.shop.lottery.service.ArtifiPrintLotteryUserLoginService;
+
+@RestController
+@RequestMapping("/artifiPrintLotteryUserLogin")
+public class ArtifiPrintLotteryUserLoginController {
+	@Resource
+	private ArtifiPrintLotteryUserLoginService artifiPrintLotteryUserLoginService;
+
+	@Resource
+	private ISMSService smsService;
+
+	@Resource
+	private IUserLoginService userLoginService;
+
+	@Resource
+	private StringRedisTemplate stringRedisTemplate;
+
+	/**
+	 * 发送短信验证码
+	 * 
+	 * @param mobileNumberParam
+	 * @return
+	 */
+	@ApiOperation(value = "发送短信验证码", notes = "发送短信验证码")
+	@PostMapping("/sendSmsCode")
+	public BaseResult<String> sendSms(@RequestBody SmsParam smsParam) {
+		return smsService.sendSmsCode(smsParam);
+	}
+
+	@ApiOperation(value = "验证码登录", notes = "发送短信验证码")
+	@PostMapping("/loginBySms")
+	public BaseResult<UserLoginDTO> loginBySms(@RequestBody UserLoginWithSmsParam userLoginMobileParam, HttpServletRequest request) {
+		// 校验手机号的合法性
+		String loginParams = JSONHelper.bean2json(userLoginMobileParam);
+		if (!RegexUtil.checkMobile(userLoginMobileParam.getMobile())) {
+			LoginLogParam loginLogParam = new LoginLogParam();
+			loginLogParam.setUserId(-1);
+			loginLogParam.setLoginType(0);
+			loginLogParam.setLoginSstatus(1);
+			loginLogParam.setLoginParams(loginParams);
+			loginLogParam.setLoginResult(MemberEnums.MOBILE_VALID_ERROR.getMsg());
+			userLoginService.loginLog(loginLogParam);
+			return ResultGenerator.genResult(MemberEnums.MOBILE_VALID_ERROR.getcode(), MemberEnums.MOBILE_VALID_ERROR.getMsg());
+		}
+		// 校验验证码的正确性
+		String mobile = userLoginMobileParam.getMobile();
+		String cacheSmsCode = smsService.getRedisSmsCode(mobile);
+		if (StringUtils.isEmpty(cacheSmsCode) || !cacheSmsCode.equals(userLoginMobileParam.getSmsCode())) {
+			LoginLogParam loginLogParam = new LoginLogParam();
+			loginLogParam.setUserId(-1);
+			loginLogParam.setLoginType(0);
+			loginLogParam.setLoginSstatus(1);
+			loginLogParam.setLoginParams(loginParams);
+			loginLogParam.setLoginResult(MemberEnums.SMSCODE_WRONG.getMsg());
+			userLoginService.loginLog(loginLogParam);
+			return ResultGenerator.genResult(MemberEnums.SMSCODE_WRONG.getcode(), MemberEnums.SMSCODE_WRONG.getMsg());
+		}
+		UserLoginDTO userLoginDTO = userLoginService.findByMobile(mobile);
+		// 校验手机号是否存在
+		if (null == userLoginDTO) {
+			LoginLogParam loginLogParam = new LoginLogParam();
+			loginLogParam.setUserId(-1);
+			loginLogParam.setLoginType(0);
+			loginLogParam.setLoginSstatus(1);
+			loginLogParam.setLoginParams(loginParams);
+			loginLogParam.setLoginResult(MemberEnums.NO_REGISTER.getMsg());
+			userLoginService.loginLog(loginLogParam);
+			return ResultGenerator.genResult(MemberEnums.NO_REGISTER.getcode(), MemberEnums.NO_REGISTER.getMsg());
+		}
+		// 将手机号存入redis,置为在线状态,过期时间为15分钟
+		stringRedisTemplate.opsForValue().set(mobile, "1", 900, TimeUnit.SECONDS);
+		// 调用怀腾接口
+		// 清空验证码
+		smsService.deleteRedisSmsCode(mobile);
+		LoginLogParam loginLogParam = new LoginLogParam();
+		loginLogParam.setUserId(Integer.parseInt(userLoginDTO.getMobile()));
+		loginLogParam.setLoginType(0);
+		loginLogParam.setLoginSstatus(0);
+		loginLogParam.setLoginParams(loginParams);
+		loginLogParam.setLoginResult(JSONHelper.bean2json(userLoginDTO));
+		userLoginService.loginLog(loginLogParam);
+		return ResultGenerator.genSuccessResult("登录成功", userLoginDTO);
+	}
+
+	@ApiOperation(value = "发送短信验证码", notes = "发送短信验证码")
+	@PostMapping("/logout")
+	public void logout(@RequestBody String mobile) {
+		stringRedisTemplate.delete(mobile);
+		// 调用怀腾接口
+	}
+}
