78f313cf787752c2d7830ac35b41db2f82ecdef0
wenhuaiteng <wenhuaiteng@126.com>
2018-11-01 15:36:53 +0800

    人工出票日志记录...

---
+++ lottery-service/src/main/java/com/dl/shop/lottery/model/DlOpLog.java
@@ -0,0 +1,45 @@
+package com.dl.shop.lottery.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "dl_log_operation")
+public class DlOpLog {
+
+	@Id
+	@Column(name = "log_id")
+	private Integer logId;
+	
+	/**
+	 * 日志类型
+	 */
+	@Column(name = "type")
+	private Integer type;
+
+	/**
+	 * 订单编号
+	 */
+	@Column(name = "order_sn")
+	private String orderSn;
+
+	/**
+	 * 操作人
+	 */
+	@Column(name = "phone")
+	private String phone;
+
+	/**
+	 * 操作类型 收银台(1收钱，2派奖)  出票系统(1已出票，2出票失败)
+	 */
+	@Column(name = "op_type")
+	private Integer opType;
+	
+	/**
+	 * 日志添加时间
+	 */
+	@Column(name = "add_time")
+	private Integer addTime;
+}
