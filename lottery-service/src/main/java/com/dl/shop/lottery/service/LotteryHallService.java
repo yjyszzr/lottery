084bd5f186e3ba1f93859a16054025113b190bfe
yjyszzr <1053195207@qq.com>
2018-11-15 17:03:10 +0800

    modified
    Change-Id: I149fe5cc6d91478a48e6d0f8bd82f09c4b996bca
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- lottery-service/src/main/java/com/dl/shop/lottery/service/LotteryHallService.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/service/LotteryHallService.java
@@ -282,7 +282,7 @@ public class LotteryHallService {
 		criteria.andCondition("end_time >", DateUtil.getCurrentTimeLong());
 		criteria.andCondition("is_show=", 1);
 		criteria.andCondition("show_position=", 0);
-		if(!StringUtils.isEmpty(hallParam.getIsTransaction()) && hallParam.getIsTransaction().equals(ProjectConstant.INFO_VERSION)) {//资讯版仅仅展示资讯版，交易版全部展示
+		if(isTransaction.equals(ProjectConstant.INFO_VERSION)) {//资讯版仅仅展示资讯版，交易版全部展示
 			criteria.andCondition("is_transaction =",Integer.valueOf(ProjectConstant.INFO_VERSION));
 		}
 		List<LotteryNavBanner> lotteryNavBanners = lotteryNavBannerMapper.selectByCondition(condition);
