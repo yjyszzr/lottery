03a01485d6691e094c601f20603c93986039ce85
zzr <zhangzirong@caixiaomi.com>
2018-11-28 17:38:10 +0800

    modified

--- lottery-api/src/main/java/com/dl/lottery/dto/DlBannerPicDTO.java
+++ lottery-api/src/main/java/com/dl/lottery/dto/DlBannerPicDTO.java
@@ -7,19 +7,23 @@ import lombok.Data;
 public class DlBannerPicDTO {
 
     @ApiModelProperty(value = "banner名称")
-    public String bannerName;
+    public String bannerName = "";
 
     @ApiModelProperty(value = "banner大图")
-    public String bannerImage;
+    public String bannerImage = "";
 
     @ApiModelProperty(value = "图片跳转链接")
-    public String bannerLink;
+    public String bannerLink = "";
 
     @ApiModelProperty(value = "banner图生效起始时间")
-    public Integer startTime;
+    public Integer startTime = 0;
 
     @ApiModelProperty(value = "banner图生效截止时间")
-    public Integer endTime;
+    public Integer endTime = 0;
+
+    public DlBannerPicDTO(){
+
+    }
 
     public DlBannerPicDTO(String bannerName,String bannerImage,String bannerLink,Integer startTime,Integer endTime){
         this.bannerName = bannerName;
