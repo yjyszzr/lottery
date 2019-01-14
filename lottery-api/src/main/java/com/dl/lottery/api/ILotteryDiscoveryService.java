516334f0a94ffad7c56336beca99a729f1b0b539
wenhuaiteng <wenhuaiteng@126.com>
2019-01-14 16:53:49 +0800

    获取每期结果...

--- lottery-api/src/main/java/com/dl/lottery/api/ILotteryDiscoveryService.java
+++ lottery-api/src/main/java/com/dl/lottery/api/ILotteryDiscoveryService.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.dl.base.result.BaseResult;
 import com.dl.lottery.dto.SZCPrizeDTO;
+import com.dl.lottery.dto.SZCResultDTO;
 import com.dl.lottery.param.DiscoveryPageParam;
+import com.dl.lottery.param.SZCQueryParam;
 
 @FeignClient(value="lottery-service")
 public interface ILotteryDiscoveryService {
 	
 	@RequestMapping(path="/discoveryPage/szcDetailListByStore", method=RequestMethod.POST)
     public BaseResult<SZCPrizeDTO> szcDetailList(@Valid @RequestBody DiscoveryPageParam param);
+	
+	
+	@RequestMapping(path="/discoveryPage/querySzcOpenPrizesByDate", method=RequestMethod.POST)
+    public BaseResult<SZCResultDTO> querySzcOpenPrizesByDate(@Valid @RequestBody SZCQueryParam param);
+	
 }
