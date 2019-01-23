aa9ea0b4e89785054b0735b843c858a13d935f47
wenhuaiteng <wenhuaiteng@126.com>
2019-01-23 14:58:32 +0800

    代码完善...

--- lottery-service/src/main/java/com/dl/shop/lottery/web/ArtifiDyQueueController.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/web/ArtifiDyQueueController.java
@@ -335,31 +335,31 @@ public class ArtifiDyQueueController {
 		return artifiDyQueueService.modifyOrderStatusV2(userId,mobile,params.getOrderSn(),params.getOrderStatus(),params.getPicUrl(),rfailMsg);
 	}
 	
-	@ApiOperation(value = "更改订单状态", notes = "更改订单状态")
-	@PostMapping("/modify")
-	public BaseResult<?> modifyOrderStatus(@RequestBody ArtifiLotteryModifyParam params){
-		int userId = SessionUtil.getUserId();
-		UserIdRealParam userIdParams = new UserIdRealParam();
-		userIdParams.setUserId(userId);
-		BaseResult<UserDTO> bR = iUserService.queryUserInfoReal(userIdParams);
-		if(bR == null || !bR.isSuccess() || bR.getData() == null) {
-			return ResultGenerator.genFailResult("查询用户信息失败");
-		}
-//		String mobile = params.getMobile();
-		String mobile = bR.getData().getMobile();
-		if(mobile == null || mobile.length() <= 0) {
-			return ResultGenerator.genFailResult("手机号码不能为空"); 
-		}
-		if(params.getOrderStatus() <= 0) {
-			return ResultGenerator.genResult(MemberEnums.ORDER_STATUS_FAILURE.getcode(),MemberEnums.ORDER_STATUS_FAILURE.getMsg());
-		}
-		//判断是否在白名单内
-		Condition c = new Condition(DlXNWhiteList.class);
-		c.createCriteria().andEqualTo("mobile",mobile);
-		List<DlXNWhiteList> xnWhiteListList = dlXNWhiteListService.findByCondition(c);
-		if (xnWhiteListList.size() == 0) {
-			return ResultGenerator.genResult(MemberEnums.NO_REGISTER.getcode(), MemberEnums.NO_REGISTER.getMsg());
-		}
-		return artifiDyQueueService.modifyOrderStatus(userId,mobile,params.getOrderSn(),params.getOrderStatus());
-	}
+//	@ApiOperation(value = "更改订单状态", notes = "更改订单状态")
+//	@PostMapping("/modify")
+//	public BaseResult<?> modifyOrderStatus(@RequestBody ArtifiLotteryModifyParam params){
+//		int userId = SessionUtil.getUserId();
+//		UserIdRealParam userIdParams = new UserIdRealParam();
+//		userIdParams.setUserId(userId);
+//		BaseResult<UserDTO> bR = iUserService.queryUserInfoReal(userIdParams);
+//		if(bR == null || !bR.isSuccess() || bR.getData() == null) {
+//			return ResultGenerator.genFailResult("查询用户信息失败");
+//		}
+////		String mobile = params.getMobile();
+//		String mobile = bR.getData().getMobile();
+//		if(mobile == null || mobile.length() <= 0) {
+//			return ResultGenerator.genFailResult("手机号码不能为空"); 
+//		}
+//		if(params.getOrderStatus() <= 0) {
+//			return ResultGenerator.genResult(MemberEnums.ORDER_STATUS_FAILURE.getcode(),MemberEnums.ORDER_STATUS_FAILURE.getMsg());
+//		}
+//		//判断是否在白名单内
+//		Condition c = new Condition(DlXNWhiteList.class);
+//		c.createCriteria().andEqualTo("mobile",mobile);
+//		List<DlXNWhiteList> xnWhiteListList = dlXNWhiteListService.findByCondition(c);
+//		if (xnWhiteListList.size() == 0) {
+//			return ResultGenerator.genResult(MemberEnums.NO_REGISTER.getcode(), MemberEnums.NO_REGISTER.getMsg());
+//		}
+//		return artifiDyQueueService.modifyOrderStatus(userId,mobile,params.getOrderSn(),params.getOrderStatus());
+//	}
 }
