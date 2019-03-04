d2239631d5d09af51529172a69be119fdd091d95
wenhuaiteng <wenhuaiteng@126.com>
2019-03-04 17:57:56 +0800

    逻辑合并...

--- lottery-service/src/main/java/com/dl/shop/lottery/service/ArtifiDyQueueService.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/service/ArtifiDyQueueService.java
@@ -394,9 +394,6 @@ public class ArtifiDyQueueService{
 					DDyArtifiPrintEntity dEntity = new DDyArtifiPrintEntity();
 					dEntity.orderSn = orderSn;
 					dEntity.status = 0;
-					if(lotteryClassifyId == null) {
-						lotteryClassifyId = -1;
-					}
 					dEntity.setLotteryClassifyId(lotteryClassifyId);
 					dyArtifiDao.addDyArtifiPrintInfo(mobile,dEntity);
 				}
