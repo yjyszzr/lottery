13ae2a209e084fc433daa06e7e0eaef777f96340
lifeilong <lifeilong@caixiaomi.com>
2019-04-25 11:25:24 +0800

    修改

--- lottery-api/src/main/java/com/dl/lottery/dto/PrintStakeResultDTO.java
+++ lottery-api/src/main/java/com/dl/lottery/dto/PrintStakeResultDTO.java
@@ -7,7 +7,10 @@ import java.io.Serializable;
 
 @Data
 public class PrintStakeResultDTO implements Serializable {
-
+	
+	@ApiModelProperty(value = "订单编号")
+	private String orderSn = "";
+	
 	@ApiModelProperty(value = "订单状态:0-待出票 1-出票成功 2-出票失败 3-待开奖  4-未中奖5-已中奖")
 	private String status = "";
 
@@ -19,5 +22,8 @@ public class PrintStakeResultDTO implements Serializable {
 
 	@ApiModelProperty(value = "赔率：格式 玩法|赛事编号|cellCode@赔率")
 	private String print_sp = "";
+	
+	@ApiModelProperty(value = "商户订单号")
+	private String merchantOrderSn = "";
 
 }
