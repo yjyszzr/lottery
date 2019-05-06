19b540c4a7f184311f0695898d617b3a76044097
zzr <zhangzirong@caixiaomi.com>
2019-05-06 17:26:02 +0800

    K

--- lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryMatchController.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryMatchController.java
@@ -1203,7 +1203,8 @@ public class LotteryMatchController {
 		dto.setTimes(param.getTimes());
 		dto.setBetType(param.getBetType());
 		dto.setPlayType(param.getPlayType());
-		dto.setPlayTypeDetail(StringUtils.join(playTypeDetail.toArray(), ","));
+		String playTypeDetailStr = StringUtils.join(playTypeDetail.toArray(), ",");
+		dto.setPlayTypeDetail(playTypeDetailStr);
 		dto.setLotteryClassifyId(param.getLotteryClassifyId());
 		dto.setLotteryPlayClassifyId(param.getLotteryPlayClassifyId());
 		dto.setBetDetailInfos(betDetailInfos);
