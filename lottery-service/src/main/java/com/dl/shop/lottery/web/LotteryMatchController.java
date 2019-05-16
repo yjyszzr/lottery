a163a90c472785779fc4d249309e8291858ef4e8
lifeilong <lifeilong@caixiaomi.com>
2019-05-16 13:25:19 +0800

    xgg

--- lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryMatchController.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryMatchController.java
@@ -567,7 +567,8 @@ public class LotteryMatchController {
     	SysConfigParam cfg = new SysConfigParam();
 		cfg.setBusinessId(67);//读取财务账号id
 		int cwuserId = iUserAccountService.queryBusinessLimit(cfg).getData()!=null?iUserAccountService.queryBusinessLimit(cfg).getData().getValue().intValue():0;
-    	if(userId==cwuserId) {//财务账号--禁止购彩
+    	logger.info("getbetinfo***********"+userId+"###"+cfg+"###"+cwuserId);
+		if(userId==cwuserId) {//财务账号--禁止购彩
     		return ResultGenerator.genResult(LotteryResultEnum.BET_USER_NOT_PAY.getCode(), LotteryResultEnum.BET_USER_NOT_PAY.getMsg());
     	}
         if(lotteryMatchService.isShutDownBet()) {
