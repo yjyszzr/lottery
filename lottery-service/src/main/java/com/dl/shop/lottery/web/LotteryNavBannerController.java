64824e9901777de7e1df2b5351e83f37eed0f7d2
lifeilong <lifeilong@caixiaomi.com>
2019-05-22 16:06:32 +0800

    xg

--- lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryNavBannerController.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryNavBannerController.java
@@ -190,7 +190,7 @@ public class LotteryNavBannerController {
         List<Object> list = new ArrayList();
         HashMap<String, Object> result = new HashMap();
         dto.setBannerImage("http://img1.efu.com.cn/upfile/fashion/photo/15310/360408.jpg");
-        dto.setBannerLink("http://t1.caixiaomi.net:9805/index/consult?id=238&frz=1&showtitle=0");
+        dto.setBannerLink("http://39.106.18.39:8765/api/lottery/freebuy/singleNote?cxmxc=scm&type=3&id=1");
         dto.setBannerName("测试活动");
         dto.setStartTime(1541779199);
         dto.setEndTime(1641779199);
