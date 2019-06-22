b201d1df85bca7f22e1bb87ec333e0c87ac31690
lifeilong <lifeilong@caixiaomi.com>
2019-06-22 12:52:49 +0800

    xg

--- lottery-service/src/main/java/com/dl/shop/lottery/service/LotteryMatchService.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/service/LotteryMatchService.java
@@ -587,7 +587,7 @@ public class LotteryMatchService extends AbstractService<LotteryMatch> {
             deviceUnique = "h5";
             log.info("h5,"+deviceUnique);
         }
-        log.info("getMatchListDTO"+matchList.size()+deviceUnique);
+        log.info("getMatchListDTO"+matchList.size()+"&&"+deviceUnique);
 		for(LotteryMatch match: matchList) {
 			Date matchTimeDate = match.getMatchTime();
 			Instant instant = matchTimeDate.toInstant();
@@ -682,10 +682,8 @@ public class LotteryMatchService extends AbstractService<LotteryMatch> {
 			matchDto.setVisitingTeamId(match.getVisitingTeamId().toString());
 			matchDto.setVisitingTeamName(match.getVisitingTeamName());
 			matchDto.setVisitingTeamRank(match.getVisitingTeamRank());
-			log.info("getMatchListDTO====23点之前"+match.getChangci());
 			List<DlJcZqMatchPlayDTO> matchPlays = matchPlayMap.get(match.getChangciId());
 			if(matchPlays == null || matchPlays.size() == 0) {
-				log.info("getMatchListDTO"+"1");
 				continue;
 			}
 			
