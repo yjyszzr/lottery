3c03427d9f5f40ffce6b4cf4a438837ec99f7161
ColorfulBikini <colorfulbikini@bogon>
2019-09-10 16:50:10 +0800

    提交

--- lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryMatchController.java
+++ lottery-service/src/main/java/com/dl/shop/lottery/web/LotteryMatchController.java
@@ -394,14 +394,14 @@ public class LotteryMatchController {
         dto.setIssue(betInfo.getIssue());
         String dtoJson = JSONHelper.bean2json(dto);
         String keyStr = "tzxx" + SessionUtil.getUserId() + "_" +System.currentTimeMillis()+"";
-//        String payToken = MD5.crypt(keyStr);
+        String payToken = MD5.crypt(keyStr);
 //        String payToken =keyStr;
        	logger.info("预设总分payToken*******dtoJson="+dtoJson);
        	logger.info("预设总分payTokenKey*******payToken="+keyStr);
        	
        	
-       	stringRedisTemplate.opsForValue().set(keyStr, dtoJson, ProjectConstant.BET_INFO_EXPIRE_TIME, TimeUnit.MINUTES);
-//        stringRedisTemplate.opsForValue().set(payToken, dtoJson, ProjectConstant.BET_INFO_EXPIRE_TIME, TimeUnit.MINUTES);
+        stringRedisTemplate.opsForValue().set(payToken, dtoJson, ProjectConstant.BET_INFO_EXPIRE_TIME, TimeUnit.MINUTES);
+        stringRedisTemplate.opsForValue().set(keyStr, dtoJson, ProjectConstant.BET_INFO_EXPIRE_TIME, TimeUnit.MINUTES);
         return ResultGenerator.genSuccessResult("success", keyStr);
     }
 
