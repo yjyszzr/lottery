7a425d99a002605d8aaf9e7ed65ab95f2d7802ad
lifeilong <lifeilong@caixiaomi.com>
2019-11-07 10:56:04 +0800

    xg

--- lottery-api/src/main/java/com/dl/lottery/param/ArtifiLotteryQueryParamV2.java
+++ lottery-api/src/main/java/com/dl/lottery/param/ArtifiLotteryQueryParamV2.java
@@ -11,4 +11,6 @@ public class ArtifiLotteryQueryParamV2 {
 	@ApiModelProperty(value="0自动刷新  1按钮手动刷新")
 	@NotEmpty
 	private Integer type; //0自动刷新  1按钮手动刷新
+	@ApiModelProperty(value="获取多少条数据")
+	private Integer pageSize;
 }
